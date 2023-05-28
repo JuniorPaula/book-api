4831ab83c19a2eb6e566defcb8423a0137bd5abe
Junior Paula <luke.junnior@icloud.com>
2023-05-28 15:38:30 -0300

    Implements methods from a book

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"bookapi/pkg/routes"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("hello, world!")
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+
+	fmt.Println("📌 listen and server on port [::]8081")
+	log.Fatal(http.ListenAndServe(":8081", r))
 }
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -9,8 +9,8 @@ import (
 var db *gorm.DB
 
 type Book struct {
-	gorm.model
-	Name        string `gorm:""json:"name"`
+	gorm.Model
+	Name        string `json:"name"`
 	Author      string `json:"author"`
 	Publication string `json:"publication"`
 }
@@ -19,4 +19,28 @@ func init() {
 	config.Connect()
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var books []Book
+	db.Find(&books)
+	return books
+}
+
+func GetBookById(id int64) (*Book, *gorm.DB) {
+	var book Book
+	db := db.Where("id=?", id).Find(&book)
+	return &book, db
+}
+
+func DeleteBook(id int64) Book {
+	var book Book
+	db.Where("id=?", id).Delete(book)
+	return book
 }
