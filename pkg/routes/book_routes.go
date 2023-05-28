2b95d704917b061fa6766b49ae119b3116573a45
Junior Paula <luke.junnior@icloud.com>
2023-05-28 14:55:40 -0300

    Implements register book store routes

--- pkg/routes/book_routes.go
+++ pkg/routes/book_routes.go
@@ -1,1 +1,16 @@
 package routes
+
+import (
+	"bookapi/pkg/handlers"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", handlers.CreateBook).Methods(http.MethodPost)
+	router.HandleFunc("/book/", handlers.GetBook).Methods(http.MethodGet)
+	router.HandleFunc("/book/{bookId}", handlers.GetBookById).Methods(http.MethodGet)
+	router.HandleFunc("/book/{bookId}", handlers.UpdateBook).Methods(http.MethodPut)
+	router.HandleFunc("/book/{bookId}", handlers.DeleteBook).Methods(http.MethodDelete)
+}
